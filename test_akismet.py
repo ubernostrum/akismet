e8fbb3334edca179aa2311140e7beb4b913629bc
James Bennett <james@b-list.org>
2017-05-23 16:17:52 -0700

    And get it working.

--- test_akismet.py
+++ test_akismet.py
@@ -4,17 +4,13 @@ import unittest
 import akismet
 
 
-API_KEY_FROM_ENV = os.getenv('TEST_AKISMET_API_KEY')
-URL_FROM_ENV = os.getenv('TEST_AKISMET_BLOG_URL')
-
-
 class AkismetConfigurationTests(unittest.TestCase):
     def setUp(self):
-        os.environ[akismet.Akismet.API_KEY_ENV_VAR] = API_KEY_FROM_ENV
-        os.environ[akismet.Akismet.BLOG_URL_ENV_VAR] = URL_FROM_ENV
+        os.environ[akismet.Akismet.API_KEY_ENV_VAR] = os.getenv('TEST_AKISMET_API_KEY')
+        os.environ[akismet.Akismet.BLOG_URL_ENV_VAR] = os.getenv('TEST_AKISMET_BLOG_URL')
 
     def tearDown(self):
-        os.unsetenv(akismet.Akisment.API_KEY_ENV_VAR)
+        os.unsetenv(akismet.Akismet.API_KEY_ENV_VAR)
         os.unsetenv(akismet.Akismet.API_KEY_ENV_VAR)
 
     def test_nothing(self):
