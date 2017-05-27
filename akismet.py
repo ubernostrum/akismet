c2374da160c4f88246a189632907e677cf1e4bba
James Bennett <james@b-list.org>
2017-05-27 03:08:31 -0700

    Support the Akismet debug header.

--- akismet.py
+++ akismet.py
@@ -5,7 +5,7 @@ import textwrap
 import requests
 
 
-__version__ = '1.0'
+__version__ = u'1.0'
 
 
 class AkismetError(Exception):
@@ -71,23 +71,23 @@ class Akismet:
     invalid.
 
     """
-    COMMENT_CHECK_URL = 'https://{}.rest.akismet.com/1.1/comment-check'
-    SUBMIT_HAM_URL = 'https://{}.rest.akismet.com/1.1/submit-ham'
-    SUBMIT_SPAM_URL = 'https://{}.rest.akismet.com/1.1/submit-spam'
-    VERIFY_KEY_URL = 'https://rest.akismet.com/1.1/verify-key'
+    COMMENT_CHECK_URL = u'https://{}.rest.akismet.com/1.1/comment-check'
+    SUBMIT_HAM_URL = u'https://{}.rest.akismet.com/1.1/submit-ham'
+    SUBMIT_SPAM_URL = u'https://{}.rest.akismet.com/1.1/submit-spam'
+    VERIFY_KEY_URL = u'https://rest.akismet.com/1.1/verify-key'
 
-    SUBMIT_SUCCESS_RESPONSE = 'Thanks for making the web a better place.'
+    SUBMIT_SUCCESS_RESPONSE = u'Thanks for making the web a better place.'
 
     OPTIONAL_KEYS = (
-        'referrer', 'permalink', 'comment_type', 'comment_author',
-        'comment_author_email', 'comment_author_url', 'comment_content',
-        'comment_date_gmt', 'comment_post_modified_gmt', 'blog_lang',
-        'blog_charset', 'user_role', 'is_test'
+        u'referrer', u'permalink', u'comment_type', u'comment_author',
+        u'comment_author_email', u'comment_author_url', u'comment_content',
+        u'comment_date_gmt', u'comment_post_modified_gmt', u'blog_lang',
+        u'blog_charset', u'user_role', u'is_test'
     )
 
     user_agent_header = {
-        'User-Agent': 'Python/{} | akismet.py/{}'.format(
-            '{}.{}'.format(*sys.version_info[:2]),
+        u'User-Agent': 'Python/{} | akismet.py/{}'.format(
+            u'{}.{}'.format(*sys.version_info[:2]),
             __version__
         )
     }
@@ -99,7 +99,7 @@ class Akismet:
                      else os.getenv('PYTHON_AKISMET_BLOG_URL'))
         if maybe_key in (None, '') or maybe_url in (None, ''):
             raise ConfigurationError(
-                textwrap.dedent('''
+                textwrap.dedent(u'''
                 Could not find full Akismet configuration.
 
                 Found API key: {}
@@ -109,7 +109,7 @@ class Akismet:
             )
         if not self.verify_key(maybe_key, maybe_url):
             raise APIKeyError(
-                'Akismet key ({}, {}) is invalid.'.format(
+                u'Akismet key ({}, {}) is invalid.'.format(
                     maybe_key, maybe_url
                 )
             )
@@ -126,9 +126,9 @@ class Akismet:
         data.
 
         """
-        data = {'blog': self.blog_url,
-                'user_ip': user_ip,
-                'user_agent': user_agent}
+        data = {u'blog': self.blog_url,
+                u'user_ip': user_ip,
+                u'user_agent': user_agent}
         for key in self.OPTIONAL_KEYS:
             if key in kwargs:
                 data[key] = kwargs[key]
@@ -136,36 +136,41 @@ class Akismet:
             endpoint.format(self.api_key),
             data=data,
             headers=self.user_agent_header)
-        return response.text
+        return response
 
     def _submission_request(self, operation, user_ip, user_agent, **kwargs):
         """
         Submits spam or ham to the Akismet API.
 
         """
-        endpoint = {'submit_spam': self.SUBMIT_SPAM_URL,
-                    'submit_ham': self.SUBMIT_HAM_URL}[operation]
-        response_text = self._api_request(
+        endpoint = {u'submit_spam': self.SUBMIT_SPAM_URL,
+                    u'submit_ham': self.SUBMIT_HAM_URL}[operation]
+        response = self._api_request(
             endpoint, user_ip, user_agent, **kwargs
         )
-        if response_text == self.SUBMIT_SUCCESS_RESPONSE:
+        if response.text == self.SUBMIT_SUCCESS_RESPONSE:
             return True
         else:
-            self._protocol_error(operation, response_text)
+            self._protocol_error(operation, response)
 
     @classmethod
-    def _protocol_error(cls, operation, message):
+    def _protocol_error(cls, operation, response):
         """
         Raises an appropriate exception for unexpected API responses.
 
         """
         raise ProtocolError(
-            textwrap.dedent('''
+            textwrap.dedent(u'''
             Received unexpected or non-standard response from Akismet API.
 
             API operation was: {}
             API response received was: {}
-            ''').format(operation, message)
+            Debug header value was: {}
+            ''').format(
+                operation,
+                response.text,
+                response.headers.get('X-akismet-debug-help')
+            )
         )
 
     @classmethod
@@ -176,9 +181,9 @@ class Akismet:
         Returns True if the key and URL are valid, False otherwise.
 
         """
-        if not blog_url.startswith(('http://', 'https://')):
+        if not blog_url.startswith((u'http://', u'https://')):
             raise ConfigurationError(
-                textwrap.dedent('''
+                textwrap.dedent(u'''
                 Invalid site URL specified: {}
 
                 Akismet requires the full URL including the leading
@@ -187,15 +192,15 @@ class Akismet:
             )
         response = requests.post(
             cls.VERIFY_KEY_URL,
-            data={'key': key, 'blog': blog_url},
+            data={u'key': key, u'blog': blog_url},
             headers=cls.user_agent_header
         )
-        if response.text == 'valid':
+        if response.text == u'valid':
             return True
-        elif response.text == 'invalid':
+        elif response.text == u'invalid':
             return False
         else:
-            cls._protocol_error('verify_key', response.text)
+            cls._protocol_error(u'verify_key', response)
 
     def comment_check(self, user_ip, user_agent, **kwargs):
         """
@@ -212,15 +217,15 @@ class Akismet:
         spam, and False for a comment that is not spam.
 
         """
-        response_text = self._api_request(
+        response = self._api_request(
             self.COMMENT_CHECK_URL, user_ip, user_agent, **kwargs
         )
-        if response_text == 'true':
+        if response.text == u'true':
             return True
-        elif response_text == 'false':
+        elif response.text == u'false':
             return False
         else:
-            self._protocol_error('comment_check', response_text)
+            self._protocol_error(u'comment_check', response)
 
     def submit_spam(self, user_ip, user_agent, **kwargs):
         """
@@ -237,7 +242,7 @@ class Akismet:
 
         """
         return self._submission_request(
-            'submit_spam', user_ip, user_agent, **kwargs
+            u'submit_spam', user_ip, user_agent, **kwargs
         )
 
     def submit_ham(self, user_ip, user_agent, **kwargs):
@@ -255,5 +260,5 @@ class Akismet:
 
         """
         return self._submission_request(
-            'submit_ham', user_ip, user_agent, **kwargs
+            u'submit_ham', user_ip, user_agent, **kwargs
         )
