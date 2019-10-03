c66c2222647f49fab56d583b5960307bcfadce8c
James Bennett <james@b-list.org>
2019-10-02 22:18:12 -0700

    Drop Python 3.4 and add Python 3.7 CI while I'm in there.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
 )
